de55e753c71ddc4df8ba89fa641746670e83bb55
Melvin <melvinalmonte@gmail.com>
2020-02-17 22:08:45 -0500

    added google console icon to title, implemented placeholder modals to buttons actions

--- frontend/src/components/sections/Sections.js
+++ frontend/src/components/sections/Sections.js
@@ -5,7 +5,7 @@ export const Sections = props => (
   <section className="section">
     <div className="container has-text-centered">
       <h1 className="title groups-title">{props.title}</h1>
-      <Button title={props.btnTitle} />
+      <Button actions={props.btnAction} title={props.btnTitle} />
       <p className="group-description">{props.description}</p>
     </div>
   </section>
